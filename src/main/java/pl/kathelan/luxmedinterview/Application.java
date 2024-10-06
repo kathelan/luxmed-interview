3e3d6dd243eeb3fbf4f2c7f3ee67d9db108ed6ff
kathelan <mateuszdeptulski@gmail.com>
2024-10-06 23:00:24 +0200

    git init

---
+++ src/main/java/pl/kathelan/luxmedinterview/Application.java
@@ -0,0 +1,13 @@
+package pl.kathelan.luxmedinterview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LuxmedInterviewApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/kathelan/luxmedinterview/LuxmedInterviewApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kathelan.luxmedinterview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuxmedInterviewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
